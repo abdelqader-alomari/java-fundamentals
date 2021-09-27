83d46bcbd95ea1afc2007563df7aecd8213ab805
abdelqader-alomari <aboud.coding@gmail.com>
2021-09-27 03:16:58 +0300

    stretch goal done

--- basics/src/Main.java
+++ basics/src/Main.java
@@ -1,38 +1,59 @@
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        pluralize ("cat",2);
-        flipNHeads(4);
+        pluralize("dog", 1);
+        pluralize("cat", 2);
+        pluralize("turtle", 0);
+        flipNHeads(2);
         clock();
     }
-    public static void pluralize(String word,int number){
-        if (number==1){
+
+    public static void pluralize(String word, int number) {
+        if (number == 1) {
             System.out.println("I own " + number + " " + word + ".");
-        }else{
+        } else {
             System.out.println("I own " + number + " " + word + "s" + ".");
         }
     }
-    public static void flipNHeads(int flips){
+
+    public static void flipNHeads(int flips) {
         int count = 0;
-        for (int i =0; i<flips; i++){
+        int heads = 0;
+        while (heads != flips) {
             double random = Math.random();
-            if (random>=0.5){
+            if (random >= 0.5) {
                 System.out.println("heads");
-                count++ ;
-            }else{
+                heads++;
+            } else {
                 System.out.println("tails");
+                heads = 0;
             }
+            count++;
         }
-        System.out.println("It took "+flips+" flips to flip "+count+" head in a row." );
+        String flipNum = count == 1 ? "flip" : "flips";
+        String headsNum = heads == 1 ? "head" : "heads";
+        System.out.println("It took " + count + " " + flipNum + " to flip " + heads + " " + headsNum + " in a row.");
     }
-    public static void clock(){
-        while(1<2){
-            LocalDateTime now = LocalDateTime.now();
-            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-            System.out.println(time);
+
+    public static void clock() {
+
+        LocalDateTime now = LocalDateTime.now();
+        int preSecond = now.getSecond();
+        float counter = 0;
+
+        for (int i = 0; i >= 0; i++) {
+            now = LocalDateTime.now();
+            int hour = now.getHour();
+            int minute = now.getMinute();
+            int second = now.getSecond();
+            counter++;
+
+            if (second - preSecond == 1) {
+                System.out.println(hour + ":" + minute + ":" + second + " " + counter / 1000000 + " MHz");
+                preSecond++;
+                counter = 0;
+            }
         }
     }
 }
-
